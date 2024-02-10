78c131f2581646e7ae756cded9a81bbc41d0d586
Maran077 <marvelmaran07@gmail.com>
2024-02-10 19:00:05 +0530

    finished ---

--- middlewere/cookie.js
+++ middlewere/cookie.js
@@ -4,8 +4,8 @@ const catchAsyncError = require("./catchAsyncError");
 const ErrorHandler = require("../error/errorHandler");
 
 const cookieVerify = catchAsyncError(async (req, res, next) => {
+  // i use query because render.com doesn't support cookie
   const { token } = req.query;
-  console.log(token);
   const decoded = jwt.verify(token, process.env.JWT_KEY);
   const user = await userModel.findById(decoded.id);
   if (!user) return new ErrorHandler("please login first", 404);
