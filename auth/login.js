95bddad5458e6f4476321b7604f8c45b79f21f6b
Maran077 <marvelmaran07@gmail.com>
2024-02-10 15:39:07 +0530

    send cookie via json

--- auth/login.js
+++ auth/login.js
@@ -18,15 +18,7 @@ const login = catchAsyncError(async (req, res, next) => {
     expiresIn: process.env.JWT_EXPRIRE,
   });
 
-  res
-    .status(200)
-    .cookie("token", token, {
-      maxAge: process.env.COOKIE_EXPRIRE * 24 * 60 * 60 * 1000,
-      secure: true,
-      sameSite: "strict",
-      domain: "http://localhost:5173",
-    })
-    .json({ success: true, msg: "successfully login", token });
+  res.status(200).json({ success: true, msg: "successfully login", token });
 });
 
 module.exports = login;
